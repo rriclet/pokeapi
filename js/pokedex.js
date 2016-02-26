9e927c7810904fcc432074fa48ed33ba30fcb9de
Robin <robinriclet@gmail.com>
2016-02-26 12:10:29 +0100

    Added getPokemonById

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,37 +1,42 @@
-angular.
-    module('app', []).
-        controller('ctrl', [ function($scope, $log, $resource, pokeapi) {
+var app = angular.module('app', ['ngResource']);
 
-        // Define initial values
-        $scope.searchPokemon;
-        $scope.list = [
-            {id:1,
-                "name": "Bulbasaur",
-                "url": ""},
-            {id:2,
-                "name": "Ivysaur",
-                "url": ""
-            },
-            {id:3,
-                "name": "Venusaur",
-                "url": ""
-            },
-            {id:4,
-                "name": "Squirtle",
-                "url": ""
-            }
-        ];
+app.factory('getPokemonById', ['$resource', function($resource) {
+    return $resource('http://pokeapi.co/api/v2/pokemon/:id/');
+}]);
 
-        $scope.choice;
+app.controller('ctrl', ['$scope', '$log', 'getPokemonById', function($scope, $log, getPokemonById) {
 
-        $scope.go = function() {
-            $log.log($scope.choice);
-        };
-    }]).
-    factory('getPokemon', [function($resource) {
-        return $resource('http://pokeapi.co/api/v2/pokemon/:id/', { id: '@_id' }, {
-            get: {
-                method: 'GET'
-            }
-        });
-    }]);
+	// Define initial values
+	$scope.inputPokemonInfo
+	$scope.currentPokemon
+	
+	$scope.list = [
+		{id:1,
+			"name": "Bulbasaur",
+			"url": ""},
+		{id:2,
+			"name": "Ivysaur",
+			"url": ""
+		},
+		{id:3,
+			"name": "Venusaur",
+			"url": ""
+		},
+		{id:4,
+			"name": "Squirtle",
+			"url": ""
+		}
+	]
+
+	$scope.choice;
+
+	$scope.go = function() {
+		var response = getPokemonById.get({id: $scope.choice.id});
+		response.$promise.then(function(value) {
+			$log.log('API called, waiting !')
+			$scope.currentPokemon = value
+		});
+	};
+	
+}]);
+	
