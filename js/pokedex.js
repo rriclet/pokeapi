f95d61dc245e47f9eef5c4c7c15578be1cd59ec9
Robin <robinriclet@gmail.com>
2016-02-26 14:14:32 +0100

    filter titleCase almost woring !

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,42 +1,38 @@
 var app = angular.module('app', ['ngResource']);
 
+app.filter('titleCase', function() {
+    return function(input) {
+      input = input || '';
+      return input.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    }
+})
+
 app.factory('getPokemonById', ['$resource', function($resource) {
     return $resource('http://pokeapi.co/api/v2/pokemon/:id/');
-}]);
+}])
 
-app.controller('ctrl', ['$scope', '$log', 'getPokemonById', function($scope, $log, getPokemonById) {
+app.factory('getPokemons', ['$resource', function($resource) {
+    return $resource('http://pokeapi.co/api/v2/pokemon/');
+}])
+
+app.controller('ctrl', ['$scope', '$log', 'getPokemonById', 'getPokemons', 'titleCase', function($scope, $log, getPokemonById, getPokemons, titleCase) {
 
 	// Define initial values
 	$scope.inputPokemonInfo
 	$scope.currentPokemon
+	$scope.pokemonList
 	
-	$scope.list = [
-		{id:1,
-			"name": "Bulbasaur",
-			"url": ""},
-		{id:2,
-			"name": "Ivysaur",
-			"url": ""
-		},
-		{id:3,
-			"name": "Venusaur",
-			"url": ""
-		},
-		{id:4,
-			"name": "Squirtle",
-			"url": ""
-		}
-	]
-
-	$scope.choice;
-
+	var pokemonListResponse = getPokemons.query();
+	pokemonListResponse.$promise.then(function(value) {
+		$scope.pokemonList = pokemonListResponse
+	})
+	
 	$scope.go = function() {
 		var response = getPokemonById.get({id: $scope.choice.id});
 		response.$promise.then(function(value) {
 			$log.log('API called, waiting !')
 			$scope.currentPokemon = value
-		});
-	};
-	
-}]);
+		})
+	}
+}])
 	
