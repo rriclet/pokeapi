16ff37d4a25346c69594610a62daccb4de9d3642
Robin Riclet <robinriclet@gmail.com>
2016-02-27 17:55:05 +0100

    Linked the controllers with the service

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,38 +1,76 @@
 var app = angular.module('app', ['ngResource'])
 
-app.filter('titleCase', function() {
-    return function(input) {
-      input = input || ''
-      return input.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()})
+// a filter to upperCase words
+app.filter('titleCase', function () {
+    return function (input) {
+        input = input || ''
+        return input.replace(/\w\S*/g, function (txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+        })
     }
 })
 
-app.factory('getPokemonById', ['$resource', function($resource) {
-    return $resource('http://pokeapi.co/api/v2/pokemon/:id/')
+app.factory('getPokemonById', ['$http', function ($http) {
+    var search = {};
+    search.get = function (url) {
+        var promise = $http.get(url).then(function (response) {
+            return response.data
+        })
+        return promise
+    }
+    return search;
 }])
 
-app.factory('getPokemons', ['$resource', function($resource) {
+app.factory('getPokemons', ['$resource', function ($resource) {
     return $resource('http://pokeapi.co/api/v2/pokemon/')
 }])
 
-app.controller('ctrl', ['$scope', '$log', 'getPokemonById', 'getPokemons', function($scope, $log, getPokemonById, getPokemons) {
+app.controller('searchPokemon', ['$scope', 'pokemonService', 'getPokemonById', 'getPokemons', function ($scope, pokemonService, getPokemonById, getPokemons) {
 
-	// Define initial values
-	$scope.inputPokemonInfo
-	$scope.currentPokemon
-	$scope.pokemonList
-	
-	var pokemonListResponse = getPokemons.query()
-	pokemonListResponse.$promise.then(function(value) {
-		$scope.pokemonList = pokemonListResponse
-	})
-	
-	$scope.go = function() {
-		var response = getPokemonById.get({id: $scope.choice.id})
-		response.$promise.then(function(value) {
-			$log.log('API called, waiting !')
-			$scope.currentPokemon = value
-		})
-	}
+    // input to filter the pokemon list
+    $scope.searchInput
+    // the chosed pokemon in the list (name & url)
+    $scope.chosedPokemon
+    // the pokemon list
+    $scope.pokemonList
+
+    getPokemons.query().$promise.then(function (value) {
+        $scope.pokemonList = value
+    })
+
+    $scope.go = function () {
+        getPokemonById.get($scope.chosedPokemon.url).then(function (response) {
+            pokemonService.setPokemon(response)
+        })
+    }
 }])
-	
+
+app.controller('displayPokemon', ['$scope', 'pokemonService', function ($scope, pokemonService) {
+
+    // varialbe used in the HTML page
+    $scope.pokemon
+
+    // watches if the pokemon in service changes
+    $scope.$watch(
+        // the watched value
+        function () {
+            return pokemonService.pokemon
+        },
+        // what to do if the value has changed
+        function (newVal, oldVal) {
+            if (typeof newVal !== undefined) {
+                $scope.pokemon = pokemonService.pokemon
+            }
+        }
+    )
+
+}])
+
+app.service('pokemonService', [function () {
+    var pokemon;
+
+    this.setPokemon = function (pokemon) {
+        this.pokemon = pokemon
+    }
+
+}])
